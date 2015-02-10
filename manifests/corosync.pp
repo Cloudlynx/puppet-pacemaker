d3125494c697658112f2d47419a3b9c681310b32
Jose Antonio COARASA PEREZ <toni-git.com@coarasa.net>
2015-02-10 16:09:23 +0100

    Fixing issue with transport and adding fence_xvm resource

--- manifests/corosync.pp
+++ manifests/corosync.pp
@@ -70,19 +70,22 @@ class pacemaker::corosync(
   }
 
   if $setup_cluster {
+
+    # This will be used when creating the cluster with udpu transport
+    if $transport == 'udpu' {
+      $additionalCommand ='&& /usr/sbin/ccs -f /etc/cluster/cluster.conf --setcman transport="udpu"'
+    } else {
+      $additionalCommand = ''  
+    }
+      
+
     exec {"Create Cluster $cluster_name":
       creates => "/etc/cluster/cluster.conf",
-      command => "/usr/sbin/pcs cluster setup --name $cluster_name $cluster_members",
+      command => "/usr/sbin/pcs cluster setup --name $cluster_name $cluster_members ${additionalCommand}",
       unless => "/usr/bin/test -f /etc/corosync/corosync.conf",
       require => Class["::pacemaker::install"],
     }
     ->
-    if $transport == 'udpu' {
-      exec { 'setting transport in cman':
-                  command => '/usr/sbin/ccs -f /etc/cluster/cluster.conf --setcman transport="udpu"',
-        }
-      }
-    ~>
     exec {"Start Cluster $cluster_name":
       unless => "/usr/sbin/pcs status >/dev/null 2>&1",
       command => "/usr/sbin/pcs cluster start --all",
